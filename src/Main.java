9e60c11b0b3461c1c1528c4974ee7a777f4d2033
Bugaga <b-ios@mail.ru>
2025-04-17 22:46:14 +0300

    git@github.com:bugaga1993/JavaDZ-1.git

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        int ticketPrice = 13676;
+
+        int colRud = 20;
+
+        if (ticketPrice < 0) {
+
+
+        } else {
+
+
+        }
+        int bonusMiles = ticketPrice / colRud;
+
+
+        System.out.println("Начисление бонусных миль - " + bonusMiles);
+
+    }
+}
